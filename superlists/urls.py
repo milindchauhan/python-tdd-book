58afc6a1808e5ecfe9ca40de9f3db8e37db099be
Milind Chauhan <milindchauhan95@gmail.com>
2020-03-30 07:17:00 +0530

    Add a URL to add an item to an existing list in urls.py

--- superlists/urls.py
+++ superlists/urls.py
@@ -21,4 +21,5 @@ urlpatterns = [
     url(r'^lists/new$', views.new_list, name='new_list'),
     url(r'^lists/(\d+)/$', views.view_list,
         name='view_list'),
+    url(r'^lists/(\d+)/add_item$', views.add_item, name='add_item'),
 ]
