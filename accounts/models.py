fad1d76b9cc2f8cd030e52c6d2d112c746a82dd3
Milind Chauhan <milindchauhan95@gmail.com>
2020-06-10 19:59:40 +0530

    add dunder str method for User model

--- accounts/models.py
+++ accounts/models.py
@@ -11,6 +11,9 @@ class User(models.Model):
     is_anonymous = False
     is_authenticated = True
 
+    def __str__(self):
+        return self.email
+
 
 class Token(models.Model):
     email = models.EmailField()
